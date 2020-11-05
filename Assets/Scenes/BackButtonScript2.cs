1dd392441e67ce116524b8d83aaa07f30c55b613
blacktop12 <eduardojeffrey@gmail.com>
2020-11-05 01:41:00 -0500

    Vistas

---
+++ Assets/Scenes/BackButtonScript2.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackButtonScript2 : MonoBehaviour
+{
+    public void backToPrevScene()
+    {
+        SceneManager.LoadScene("Scene2");
+    }
+}
---
+++ Assets/Scenes/SceneSelectScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSelectScript : MonoBehaviour
+{
+    public void selectScene()
+    {
+        switch (this.gameObject.name)
+        {
+            case "BttnComencemos":
+            SceneManager.LoadScene ("Scene1");
+            break;
+            case "BttnInfo":
+            SceneManager.LoadScene ("Scene2");
+            break;
+            case "BttnPf":
+            SceneManager.LoadScene ("Scene3");
+            break;
+            case "BttnModel3D":
+            SceneManager.LoadScene ("Scene4");
+            break;
+        }
+    }
+}
